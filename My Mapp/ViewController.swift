af1687b025a1f839e3c9cce4a0db4e978b77f653
Dinmukhamed Arystanbekov <lleonov839gmail.com@MacBook-Air-Dinmukhamed.local>
2024-02-08 15:17:11 +0600

    first commit

---
+++ My Mapp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  My Mapp
+//
+//  Created by Dinmukhamed Arystanbekov on 2/7/24.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var MyMapp: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
